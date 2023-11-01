d57266354f9e8d8e25de5656819dfc44f2253063
ialarmedalien <ialarmedalien@gmail.com>
2023-11-01 11:47:42 -0700

    un-indent all the methods in the test classes

--- test/SampleSetSearch_test.py
+++ test/SampleSetSearch_test.py
@@ -21,89 +21,89 @@ def compare_samples(s1, s2):
 
 
 class SetAPITest(BaseTestClass):
-    @classmethod
-    def prepare_data(cls: BaseTestClass) -> None:
-        # nothing to do here
-        pass
+@classmethod
+def prepare_data(cls: BaseTestClass) -> None:
+    # nothing to do here
+    pass
 
-    def test_param_error_conditions(self):
-        # test without ref argument
-        with pytest.raises(
-            ValueError, match="Argument 'ref' must be specified, 'ref' = 'None'"
-        ):
-            self.set_api_client.sample_set_to_samples_info(
-                self.ctx, {"start": 0, "limit": 10}
-            )
+def test_param_error_conditions(self):
+    # test without ref argument
+    with pytest.raises(
+        ValueError, match="Argument 'ref' must be specified, 'ref' = 'None'"
+    ):
+        self.set_api_client.sample_set_to_samples_info(
+            self.ctx, {"start": 0, "limit": 10}
+        )
 
-    def test_param_error_conditions_empty_params(self):
-        with pytest.raises(
-            ValueError, match="Argument 'ref' must be specified, 'ref' = 'None'"
-        ):
-            self.set_api_client.sample_set_to_samples_info(self.ctx, {})
+def test_param_error_conditions_empty_params(self):
+    with pytest.raises(
+        ValueError, match="Argument 'ref' must be specified, 'ref' = 'None'"
+    ):
+        self.set_api_client.sample_set_to_samples_info(self.ctx, {})
 
-    @unittest.skip("only particular users have permission to search")
-    # test_sample_set_to_sample_info
-    def test_sample_set_to_sample_info(self):
-        # test defaults of "start" and "limit" variables
-        ret = self.set_api_client.sample_set_to_samples_info(
-            self.ctx,
-            {
-                "ref": SAMPLE_SET_REF,
-            },
-        )[0]
-        with open("data/sample_set_search_compare.json") as f:
-            compare_to = json.load(f)
-        compare_samples(ret, compare_to)
+@unittest.skip("only particular users have permission to search")
+# test_sample_set_to_sample_info
+def test_sample_set_to_sample_info(self):
+    # test defaults of "start" and "limit" variables
+    ret = self.set_api_client.sample_set_to_samples_info(
+        self.ctx,
+        {
+            "ref": SAMPLE_SET_REF,
+        },
+    )[0]
+    with open("data/sample_set_search_compare.json") as f:
+        compare_to = json.load(f)
+    compare_samples(ret, compare_to)
 
-    @unittest.skip("only particular users have permission to search")
-    # skipped because only particular users have permission to search
-    def test_query_search(self):
-        ret = self.set_api_client.sample_set_to_samples_info(
-            self.ctx,
-            {"ref": SAMPLE_SET_REF, "start": 0, "limit": 10, "query": "Georgia"},
-        )[0]
-        with open("data/sample_set_search_compare.json") as f:
-            compare_to = json.load(f)
-        # get the samples with state_province 'Georgia' only
-        compare_to["samples"] = [
-            s
-            for s in compare_to["samples"]
-            if s.get("state_province") and s["state_province"][0] == "Georgia"
-        ]
-        compare_to["num_found"] = len(compare_to["samples"])
-        compare_samples(ret, compare_to)
+@unittest.skip("only particular users have permission to search")
+# skipped because only particular users have permission to search
+def test_query_search(self):
+    ret = self.set_api_client.sample_set_to_samples_info(
+        self.ctx,
+        {"ref": SAMPLE_SET_REF, "start": 0, "limit": 10, "query": "Georgia"},
+    )[0]
+    with open("data/sample_set_search_compare.json") as f:
+        compare_to = json.load(f)
+    # get the samples with state_province 'Georgia' only
+    compare_to["samples"] = [
+        s
+        for s in compare_to["samples"]
+        if s.get("state_province") and s["state_province"][0] == "Georgia"
+    ]
+    compare_to["num_found"] = len(compare_to["samples"])
+    compare_samples(ret, compare_to)
 
-    @unittest.skip("only particular users have permission to search")
-    # skipped because only particular users have permission to search
-    def test_prefix_query_search(self):
-        ret = self.set_api_client.sample_set_to_samples_info(
-            self.ctx, {"ref": SAMPLE_SET_REF, "query": "Germa"}
-        )[0]
-        with open("data/sample_set_search_compare.json") as f:
-            compare_to = json.load(f)
-        # get the samples with country 'Germany' only
-        compare_to["samples"] = [
-            s
-            for s in compare_to["samples"]
-            if s.get("country") and s["country"][0] == "Germany"
-        ]
-        compare_to["num_found"] = len(compare_to["samples"])
-        compare_samples(ret, compare_to)
+@unittest.skip("only particular users have permission to search")
+# skipped because only particular users have permission to search
+def test_prefix_query_search(self):
+    ret = self.set_api_client.sample_set_to_samples_info(
+        self.ctx, {"ref": SAMPLE_SET_REF, "query": "Germa"}
+    )[0]
+    with open("data/sample_set_search_compare.json") as f:
+        compare_to = json.load(f)
+    # get the samples with country 'Germany' only
+    compare_to["samples"] = [
+        s
+        for s in compare_to["samples"]
+        if s.get("country") and s["country"][0] == "Germany"
+    ]
+    compare_to["num_found"] = len(compare_to["samples"])
+    compare_samples(ret, compare_to)
 
-    @unittest.skip("only particular users have permission to search")
-    # skipped because only particular users have permission to search
-    def test_prefix_query_search_2(self):
-        ret = self.set_api_client.sample_set_to_samples_info(
-            self.ctx, {"ref": SAMPLE_SET_REF, "query": "Ge"}
-        )[0]
-        with open("data/sample_set_search_compare.json") as f:
-            compare_to = json.load(f)
-        # get the samples with country 'Germany' only
-        compare_to["samples"] = [
-            s
-            for s in compare_to["samples"]
-            if (s.get("country") and s["country"][0] == "Germany")
-            or (s.get("state_province") and s["state_province"][0] == "Georgia")
-        ]
-        compare_to["num_found"] = len(compare_to["samples"])
-        compare_samples(ret, compare_to)
+@unittest.skip("only particular users have permission to search")
+# skipped because only particular users have permission to search
+def test_prefix_query_search_2(self):
+    ret = self.set_api_client.sample_set_to_samples_info(
+        self.ctx, {"ref": SAMPLE_SET_REF, "query": "Ge"}
+    )[0]
+    with open("data/sample_set_search_compare.json") as f:
+        compare_to = json.load(f)
+    # get the samples with country 'Germany' only
+    compare_to["samples"] = [
+        s
+        for s in compare_to["samples"]
+        if (s.get("country") and s["country"][0] == "Germany")
+        or (s.get("state_province") and s["state_province"][0] == "Georgia")
+    ]
+    compare_to["num_found"] = len(compare_to["samples"])
+    compare_samples(ret, compare_to)
